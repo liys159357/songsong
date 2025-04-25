3d85cea26bdc51b5d2685d5a4f8e6eb5379ffe92
songsong <songsong@123.com>
2025-04-25 11:04:46 +0800

    新增员工业务代码开发

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -3,11 +3,11 @@ package com.sky.context;
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
-
+    //存
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
-
+    //取
     public static Long getCurrentId() {
         return threadLocal.get();
     }
